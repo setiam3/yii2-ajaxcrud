508cce7e353f4b3f92cd97ca353f5274fc655792
setiam3 <setiam3@gmail.com>
2020-09-15 21:37:27 +0700

    Update _columns.php

--- src/generators/default/views/_columns.php
+++ src/generators/default/views/_columns.php
@@ -9,7 +9,8 @@ use yii\helpers\StringHelper;
 
 /* @var $this yii\web\View */
 /* @var $generator yii\gii\generators\crud\Generator */
-
+$controllerClass = StringHelper::basename($generator->controllerClass);
+$controllername=strtolower(str_replace("Controller","",$controllerClass));
 $modelClass = StringHelper::basename($generator->modelClass);
 $urlParams = $generator->generateUrlParams();
 $nameAttribute = $generator->getNameAttribute();
@@ -19,7 +20,7 @@ echo "<?php\n";
 
 ?>
 use yii\helpers\Url;
-
+use yii\helpers\Html;
 return [
     [
         'class' => 'kartik\grid\CheckboxColumn',
@@ -57,14 +58,30 @@ return [
         'urlCreator' => function($action, $model, $key, $index) { 
                 return Url::to([$action,'<?=substr($actionParams,1)?>'=>$key]);
         },
-        'viewOptions'=>['role'=>'modal-remote','title'=>'View','data-toggle'=>'tooltip'],
-        'updateOptions'=>['role'=>'modal-remote','title'=>'Update', 'data-toggle'=>'tooltip'],
-        'deleteOptions'=>['role'=>'modal-remote','title'=>'Delete', 
-                          'data-confirm'=>false, 'data-method'=>false,// for overide yii data api
-                          'data-request-method'=>'post',
-                          'data-toggle'=>'tooltip',
-                          'data-confirm-title'=>'Are you sure?',
-                          'data-confirm-message'=>'Are you sure want to delete this item'], 
+        'buttons' => [
+            'view' => function ($url, $model) {
+                $idmodal=md5($model::className());
+                $t ='@web/<?php echo $controllername?>/view?id='.$model-><?php echo $generator->modelClass::primaryKey()[0]?>;
+                return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', Url::to($t), ['role' => 'modal-remote','data-target'=>'#'.$idmodal, 'title' => 'View', 'data-toggle' => 'tooltip']);
+            },
+            'update' => function ($url, $model) {
+                $idmodal=md5($model::className());
+                $t = '@web/<?php echo $controllername?>/update?id=' . $model-><?php echo $generator->modelClass::primaryKey()[0]?>;
+                return Html::a('<span class="glyphicon glyphicon-pencil"></span>', Url::to($t), ['role' => 'modal-remote', 'data-target'=>'#'.$idmodal, 'title' => 'Update', 'data-toggle' => 'tooltip']);
+            },
+            'delete' => function ($url, $model) {
+                $idmodal=md5($model::className());
+                $t = '@web/<?php echo $controllername?>/delete?id=' . $model-><?php echo $generator->modelClass::primaryKey()[0]?>;
+                return Html::a('<span class="glyphicon glyphicon-trash"></span>', Url::to($t), [
+                    'role' => 'modal-remote', 'data-target'=>'#'.$idmodal, 'title' => 'Delete',
+                    'data-confirm' => false, 'data-method' => false,
+                    'data-request-method' => 'post',
+                    'data-toggle' => 'tooltip',
+                    'data-confirm-title' => 'Are you sure?',
+                    'data-confirm-message' => 'Are you sure want to delete this item'
+                ]);
+            },
+        ],
     ],
 
 ];   

6e2067fbc5720731689abcfcf1df0be7c6504ca7
setiam3 <setiam3@gmail.com>
2020-09-15 21:42:34 +0700

    Update index.php

--- src/generators/default/views/index.php
+++ src/generators/default/views/index.php
@@ -12,14 +12,16 @@ use yii\helpers\Html;
 
 $urlParams = $generator->generateUrlParams();
 $nameAttribute = $generator->getNameAttribute();
+$controllerClass = StringHelper::basename($generator->controllerClass);
+$controllername=strtolower(str_replace("Controller","",$controllerClass));
 echo "<?php\n";
 ?>
 use yii\helpers\Url;
 use yii\helpers\Html;
 use yii\bootstrap\Modal;
 use kartik\grid\GridView;
-use johnitvn\ajaxcrud\CrudAsset; 
-use johnitvn\ajaxcrud\BulkButtonWidget;
+use setiam3\ajaxcrud\CrudAsset; 
+use setiam3\ajaxcrud\BulkButtonWidget;
 
 /* @var $this yii\web\View */
 <?= !empty($generator->searchModelClass) ? "/* @var \$searchModel " . ltrim($generator->searchModelClass, '\\') . " */\n" : '' ?>
@@ -29,20 +31,20 @@ $this->title = <?= $generator->generateString(Inflector::pluralize(Inflector::ca
 $this->params['breadcrumbs'][] = $this->title;
 
 CrudAsset::register($this);
-
+$idmodal=md5($dataProvider->query->modelClass);
 ?>
 <div class="<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>-index">
     <div id="ajaxCrudDatatable">
         <?="<?="?>GridView::widget([
-            'id'=>'crud-datatable',
+            'id'=>'crud-datatable'.$idmodal,
             'dataProvider' => $dataProvider,
             'filterModel' => $searchModel,
             'pjax'=>true,
             'columns' => require(__DIR__.'/_columns.php'),
             'toolbar'=> [
                 ['content'=>
-                    Html::a('<i class="glyphicon glyphicon-plus"></i>', ['create'],
-                    ['role'=>'modal-remote','title'=> 'Create new <?= Inflector::pluralize(Inflector::camel2words(StringHelper::basename($generator->modelClass))) ?>','class'=>'btn btn-default']).
+                    Html::a('<i class="glyphicon glyphicon-plus"></i>', ['<?=$controllername?>/create'],
+                    ['role'=>'modal-remote','data-target'=>'#'.$idmodal,'title'=> 'Create new <?= Inflector::pluralize(Inflector::camel2words(StringHelper::basename($generator->modelClass))) ?>','class'=>'btn btn-default']).
                     Html::a('<i class="glyphicon glyphicon-repeat"></i>', [''],
                     ['data-pjax'=>1, 'class'=>'btn btn-default', 'title'=>'Reset Grid']).
                     '{toggleData}'.
@@ -62,6 +64,7 @@ CrudAsset::register($this);
                                 [
                                     "class"=>"btn btn-danger btn-xs",
                                     'role'=>'modal-remote-bulk',
+                                    'data-target'=>'#'.$idmodal,
                                     'data-confirm'=>false, 'data-method'=>false,// for overide yii data api
                                     'data-request-method'=>'post',
                                     'data-confirm-title'=>'Are you sure?',
@@ -74,7 +77,8 @@ CrudAsset::register($this);
     </div>
 </div>
 <?='<?php Modal::begin([
-    "id"=>"ajaxCrudModal",
+    "id"=>$idmodal,
+    "size"=>"modal-lg",
     "footer"=>"",// always need it for jquery plugin
 ])?>'."\n"?>
 <?='<?php Modal::end(); ?>'?>
